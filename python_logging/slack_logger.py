d62b0161f5879157995e81a9aa1714d72e0e9992
Abhi Agarwal <goldcobrapro@gmail.com>
2017-11-30 12:46:22 -0500

    Appease Mypy

--- python_logging/slack_logger.py
+++ python_logging/slack_logger.py
@@ -1,43 +1,45 @@
 """main logger"""
 import logging
 import os
-from typing import Optional
 import sys
 
 from .formatter import SlackFormatter
 from .handler import SlackHandler
 
-def make_slack_logger(
-        app_name: str,
-        formatter: Optional[logging.Formatter] = None
-    ) -> logging.Logger:
+def make_slack_logger(app_name: str) -> logging.Logger:
     """makes new slack logger"""
     slack_logger: logging.Logger = logging.getLogger(app_name)
+    slack_logger.setLevel(logging.NOTSET)
 
     slack_handler: logging.Handler = SlackHandler(os.environ["slack_webhook_url"])
-    slack_handler.setLevel(logging.INFO)
-    slack_formatter: logging.Formatter = SlackFormatter(app_name) if (
-        formatter is None
-    ) else formatter
+    slack_formatter: logging.Formatter = SlackFormatter(app_name)
     slack_handler.setFormatter(slack_formatter)
+    slack_handler.setLevel(logging.INFO)
 
     file_handler: logging.Handler = logging.FileHandler(
         os.path.join(os.sep, "var", "log", app_name + ".log")
     )
-    file_handler.setLevel(logging.DEBUG)
     file_formatter: logging.Formatter = logging.Formatter(
         fmt='%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
     )
     file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.DEBUG)
 
     slack_logger.addHandler(slack_handler)
     slack_logger.addHandler(file_handler)
 
-    def log_excepthook(exctype, value, traceback):
+    from traceback import format_exception
+    from types import TracebackType
+    from typing import Type
+    def log_excepthook(
+            type_: Type[BaseException],
+            value: BaseException,
+            traceback: TracebackType
+        ) -> None:
         """override excepthook to log"""
-        import traceback
-        traceback: str = traceback.print_exception(exctype, value, traceback)
-        slack_logger.critical(traceback)
+        fmt_trace = format_exception(type_, value, traceback)
+        slack_logger.critical("".join(fmt_trace))
+        return
 
     sys.excepthook = log_excepthook
 
