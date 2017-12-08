bc493638d7ba29e495aaa7f817df69437f6faf26
Abhi Agarwal <goldcobrapro@gmail.com>
2017-12-08 16:05:55 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,34 @@
 """setup"""
-from setuptools import setup
+from typing import List
+
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as file:
+    requirements: List[str] = file.read().splitlines()
 
 setup(
     name="slack_python_logging",
-    version="0.1.0",
+    description="Module for logging to a Slack Channel with Webhooks",
+    version="1.1.0",
+    python_requires='>=3',
+    install_requires=requirements,
+    packages=find_packages(),
+    package_dir={"": "slack_python_logging"},
+    # metadata
     author="Abhi Agarwal",
     author_email="abhi@neoliber.al",
-    packages=["slack_python_logging"],
     url="https://github.com/neoliberal/slack_python_logging",
-    license="LICENSE",
-    description="Module for logging to a Slack Channel with Webhooks",
-    python_requires='>=3',
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers"
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    keywords="reddit slack python_logging",
 )
