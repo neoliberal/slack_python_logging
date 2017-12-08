4dc9feec58d4e673fcbaafd16a17f1285cc07b99
Abhi Agarwal <goldcobrapro@gmail.com>
2017-12-08 09:44:19 -0500

    Add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""setup"""
+from distutils.core import setup
+
+setup(
+    name="slackbot_python_logger",
+    version="0.1.0",
+    author="Abhi Agarwal",
+    author_email="abhi@neoliber.al",
+    packages=["slack_python_logging"],
+    url="https://github.com/neoliberal/slack_python_logging",
+    license="LICENSE",
+    description="Module for logging to a Slack Channel with Webhooks",
+)
