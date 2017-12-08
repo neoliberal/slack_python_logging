4063513046a149bfa15772b1fabb6256a395f4b0
Abhi Agarwal <goldcobrapro@gmail.com>
2017-12-08 11:21:26 -0500

    I am bad at this

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-    name="slackbot_python_logger",
+    name="slack_python_logging",
     version="0.1.0",
     author="Abhi Agarwal",
     author_email="abhi@neoliber.al",
