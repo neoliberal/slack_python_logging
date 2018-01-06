967181dfd64bbc16586f322f1e235da17b9aba39
Abhi Agarwal <goldenercobra@gmail.com>
2018-01-06 16:34:13 -0500

    Conditional systemd imports

--- slack_python_logging/slack_logger.py
+++ slack_python_logging/slack_logger.py
@@ -3,7 +3,6 @@ import logging
 import os
 import sys
 
-from systemd import journal
 
 from .formatter import SlackFormatter
 from .handler import SlackHandler
@@ -17,16 +16,26 @@ def initialize(app_name: str, webhook_url=os.environ["slack_webhook_url"]) -> lo
     slack_handler.setLevel(logging.INFO)
     slack_formatter: logging.Formatter = SlackFormatter(app_name)
     slack_handler.setFormatter(slack_formatter)
-
-    journal_handler: logging.Handler = journal.JournalHandler(SYSLOG_IDENTIFIER=app_name)
-    journal_handler.setLevel(logging.DEBUG)
-    journal_formatter: logging.Formatter = logging.Formatter(
-        fmt='%(asctime)s %(levelname)-8s %(lineno)-4d %(message)s'
-    )
-    journal_handler.setFormatter(journal_formatter)
-
     slack_logger.addHandler(slack_handler)
-    slack_logger.addHandler(journal_handler)
+
+    try:
+        from systemd import journal
+    except ImportError:
+        file_handler: logging.Handler = logging.FileHandler(f"{app_name}.log")
+        file_handler.setLevel(logging.DEBUG)
+        file_formatter: logging.Formatter = logging.Formatter(
+            fmt='%(asctime)s %(levelname)-8s %(lineno)-4s %(message)s'
+        )
+        file_handler.setFormatter(file_formatter)
+        slack_logger.addHandler(file_handler)
+    else:
+        journal_handler: logging.Handler = journal.JournalHandler(SYSLOG_IDENTIFIER=app_name)
+        journal_handler.setLevel(logging.DEBUG)
+        journal_formatter: logging.Formatter = logging.Formatter(
+            fmt='%(asctime)s %(levelname)-8s %(lineno)-4s %(message)s'
+        )
+        journal_handler.setFormatter(journal_formatter)
+        slack_logger.addHandler(journal_handler)
 
     from types import TracebackType
     from typing import Type
