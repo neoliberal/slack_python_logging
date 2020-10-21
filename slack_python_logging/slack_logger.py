ab2aca0a64ce1958e73e31e37a86a68397ef7851
jenbanim <jenbanim@gmail.com>
2020-10-21 07:30:45 +0000

    Take stream/slack log levels as optional arguments
    This makes it easy for each bot that uses this module to set the log
    levels individually.

--- slack_python_logging/slack_logger.py
+++ slack_python_logging/slack_logger.py
@@ -2,22 +2,29 @@
 import logging
 import os
 import sys
+from typing import Union
 
 from .formatter import SlackFormatter
 from .handler import SlackHandler
 
 
-def initialize(app_name: str) -> logging.Logger:
+def initialize(
+        app_name: str,
+        stream_loglevel: Union[str, int] = "WARNING",
+        slack_loglevel: Union[str, int] = "CRITICAL",
+    ) -> logging.Logger:
     """makes new slack logger"""
+
     # The logger object to which we will add stream/slack handlers
     slack_logger: logging.Logger = logging.getLogger(app_name)
+    slack_logger.setLevel("DEBUG") # Log everything that the handlers emit
     formatter: logging.Formatter = logging.Formatter(
         fmt='%(asctime)s %(levelname)-8s %(lineno)-4s %(message)s'
     )
 
     # Log everything to stdout/stderr
     stream_handler: logging.Handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setLevel(stream_loglevel)
     stream_handler.setFormatter(formatter)
     slack_logger.addHandler(stream_handler)
 
@@ -41,7 +48,7 @@ def initialize(app_name: str) -> logging.Logger:
     try:
         webhook_url: str = os.environ["slack_webhook_url"]
         slack_handler: logging.Handler = SlackHandler(webhook_url)
-        slack_handler.setLevel(logging.ERROR)
+        slack_handler.setLevel(slack_loglevel)
         slack_formatter: logging.Formatter = SlackFormatter(app_name)
         slack_handler.setFormatter(slack_formatter)
         slack_logger.addHandler(slack_handler)
